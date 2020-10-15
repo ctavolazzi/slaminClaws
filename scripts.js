f41beb7cd69416e0f742c89d611b8cb8ebad6189
Christopher Tavolazzi <61925030+ctavolazzi@users.noreply.github.com>
2020-10-14 21:30:54 -0700

    Oct 14 Update - update to current build
    Added a getRandomInt function from this StackOverflow question (thank you oh great "Ionuț G. Stan") - https://bit.ly/3dqRVQl
    
    Added the ability to generate random Claw objects. 
    
    Changed the function that generated new Claws to use a randomly generated flavor name and a randomly generated point value by default if no parameters are passed in.
    
    Lots of JS work and I figured I might as well upload the Index file I've been working on too, just in case.
    
    This doesn't add any functionality to the game on the user end, but adds a ton of stuff to play with on the developer end. I'm looking forward to playing around with the random  claw generation to get some inspiration for possible game mechanics.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 function main () {
   console.log("Game Begin")
+
+  //change the display message
   document.getElementById("messageBox").innerHTML = "SLAM THOSE CLAWS!"; 
-  // add a new button to slam claws
+  
+  //add new buttons to slam claws
   document.getElementById("controls").innerHTML = '<button id="team1SlamButton" onclick="TEAM1.slam(MANGO_CLAW)">Team 1: Slam A Claw!</button>' + '<br>' + '<button id="team2SlamButton" onclick="TEAM2.slam(MANGO_CLAW)">Team 2 Slam a Claw!</button>'
+  
+  //ends the game
   setTimeout(function () {
     document.getElementById("controls").innerHTML = '';
     document.getElementById("messageBox").innerHTML = "GAME OVER"; 
     document.getElementById("pointsTotals").innerHTML = "Team 1 Points: " + TEAM1.points + "<br><br>" + "Team 2 Points: " + TEAM2.points;
-  }, 3000);
-    // disable game buttons
+  }, 5000);
 }
 
 
@@ -18,6 +22,7 @@ function main () {
 const TEAM1 = {
     name: "Team 1", 
     points: 0,
+    flavorTracker: [],
     /*
     get points() {
       return this.points;
@@ -28,13 +33,14 @@ const TEAM1 = {
     */
     slam (claw) {
       this.points += claw.points;
-      console.log(this.points);
+      this.flavorTracker.push(claw.flavor);
     }
   };
 
 const TEAM2 = {
     name: "Team 2", 
     points: 0,
+    flavorTracker: [],
     /*
     get points() {
       return this.points;
@@ -45,17 +51,53 @@ const TEAM2 = {
     */
     slam (claw) {
       this.points += claw.points;
-      console.log(this.points);
+      this.flavorTracker.push(claw.flavor);
     }
   };
 
 
+// CLAWS
+
+//claw data structure
+
+const CLAW_DATA = {
+  flavors: [
+    "Mango",
+    "Black Cherry",
+    "Raspberry",
+    "Lime",
+    "Lemon",
+    "Grapefruit",
+    "Tangerine",
+    "Watermelon"
+  ]
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomFlavorGen () {
+  return CLAW_DATA.flavors[getRandomInt(0, CLAW_DATA.flavors.length - 1)]
+}
+
+
 // claw constructor
 
-function Claw (flavor, points) {
+
+function Claw (points = getRandomInt(1, 10), flavor = randomFlavorGen()) {
   this.flavor = flavor;
   this.points = points;
 }
 
-const MANGO_CLAW = new Claw("Mango", 5);
+function slamALotOfClaws (teamName, num) {
+  for(let i= 0; i<num; i++) {
+    teamName.slam(new Claw());
+  }
+  return teamName
+}
 
+console.log(slamALotOfClaws(TEAM1, 100))
+console.log(slamALotOfClaws(TEAM2, 100))
